1aa73a37f69e548237587a293b035ec09cdbe970
Musa Jibril <musjib999@gmail.com>
2022-10-31 17:13:44 +0100

    account screen and message bubble done

--- lib/app/presentation/investor/investor.dart
+++ lib/app/presentation/investor/investor.dart
@@ -1,2 +1,3 @@
 export 'home/home.dart';
 export 'investment/investment.dart';
+export 'profile/profile.dart';
---
+++ lib/app/presentation/investor/profile/screens/account.dart
@@ -0,0 +1,82 @@
+import 'package:blusearch/app/index.dart';
+
+class Account extends StatelessWidget {
+  const Account({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Account'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(12.sp),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 10.sp),
+            const Center(
+              child:  CircleAvatar(
+                radius: 40.0,
+                backgroundColor: AppColors.inputGrey,
+                child: Icon(Ionicons.person, color: AppColors.white, size: 40,),
+              ),
+            ),
+            SizedBox(height: 7.sp),
+            Center(
+              child: Text('Faith Oluwafemi', style: GoogleFonts.montserrat(fontSize: 20, fontWeight: FontWeight.w600),),
+            ),
+            SizedBox(height: 40.sp),
+             ListTile(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                  color: AppColors.inputGrey,
+                ),
+                borderRadius: BorderRadius.circular(8.0)
+              ),
+              leading: const Icon(Ionicons.person_circle),
+              title: const Text('Personal Information'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+            ),
+            SizedBox(height: 15.sp),
+            ListTile(
+              shape: RoundedRectangleBorder(
+                  side: const BorderSide(
+                    color: AppColors.inputGrey,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0)
+              ),
+              leading: const Icon(Icons.manage_accounts_sharp),
+              title: const Text('Account Settings'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+            ),
+            SizedBox(height: 15.sp),
+            ListTile(
+              shape: RoundedRectangleBorder(
+                  side: const BorderSide(
+                    color: AppColors.inputGrey,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0)
+              ),
+              leading: const Icon(Ionicons.card),
+              title: const Text('Bank and Card Details'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+            ),
+            SizedBox(height: 15.sp),
+            ListTile(
+              shape: RoundedRectangleBorder(
+                  side: const BorderSide(
+                    color: AppColors.errorRed,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0)
+              ),
+              leading: const Icon(Icons.logout, color: AppColors.errorRed,),
+              title: const Text('Logout', style: TextStyle(color: AppColors.errorRed),),
+              trailing: const Icon(Icons.arrow_forward_ios, color: AppColors.errorRed,),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
